9760050efbfa8166e2c91c707cfc280a55664463
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-02 19:51:37 +0530

    comp a completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Home from './components/Home';
 import phone from "./assets/iphone.png";
 import lap from "./assets/laptop.png";
 import ipad from "./assets/tab.png";
+import CompA from './components/CompA';
 
 const App = () => {
+  let a = 10;
   return (
     <div>
-      <Home name="Iphone" price="$989" desc="This is Iphone 13 Pro" img={phone} />
+      {/* <Home name="Iphone" price="$989" desc="This is Iphone 13 Pro" img={phone} />
         <Home name="Acer" price="$763" desc="This is Acer Laptop" img={lap} />
-          <Home name="Ipad" price="$788" desc="This is Apple Ipad" img={ipad} />
+          <Home name="Ipad" price="$788" desc="This is Apple Ipad" img={ipad} /> */}
+
+        <CompA a={a} />
     </div>
   );
 };
---
+++ src/components/CompA.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CompA = ({a}) => {
+    return (
+        <div>
+            <h1>Component</h1>
+            <h1>The value of a is :{a}</h1>
+        </div>
+    );
+};
+
+export default CompA;
