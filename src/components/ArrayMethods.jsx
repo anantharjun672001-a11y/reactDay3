d7a59d54f2846a0b3706cdedc7095920d31dc8e0
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-03 10:08:39 +0530

    addUser completed

--- src/components/ArrayMethods.jsx
+++ src/components/ArrayMethods.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 
 const ArrayMethods = () => {
     const users = ["John", "Doe", "Alice", "Smith", "Adam", "Jack", "Shoe"]; //7
+    const addUser=()=>{
+        const ind = users.push("Sparrow");     //We pushed the array of sparrow
+        console.log(ind);                       //log print the index value
+        console.log("Add User",users[ind-1]);    // log print the Add User & the last array
+        users.forEach((ele) =>console.log(ele) ); //for each print the all arrays in loop
+
+        const userIndex = users.indexOf("Daniel")  // the indexOf array Daniel 
+        if (userIndex === -1) {                         // check the indexOf are already in or not
+            console.log("User is not in the array");    //print the unknown array
+            const lastEle = users.push("Daniel");       //push into the array
+            console.log(lastEle);
+            console.log("Add user", users[lastEle - 1]);
+            users.forEach((ele) => console.log(ele));
+
+         }
+        
+    }
+
     return (
         <div>
             <h1>Array</h1>
@@ -16,7 +34,7 @@ const ArrayMethods = () => {
                 })
             }
             </ul>
-            <button>Add</button>
+            <button onClick={addUser}>Add</button>
             <button>Remove</button>
             <button>Splice</button>
             <button>Slice</button>
